071a35bc2d5a87eb1c5951dfe155d13ac43294d6
tihonov.a <tihonov.a@bk.ru>
2022-01-31 14:45:57 +0300

    1

---
+++ 4.py
@@ -0,0 +1,9 @@
+a = int(input('введи а='))
+b = int(input('введи b='))
+if a > b:
+    print('а больше b')
+else:  # а меньше или равно b
+    if a == b:
+        print('числа равны')
+    else:
+        print('a<b')
